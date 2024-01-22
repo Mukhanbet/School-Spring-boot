583247443db8a872146ebb10b32252716e7854e5
Mukhanbet <nurmuhanbet2004@icloud.com>
2024-01-22 11:25:26 +0600

    The whole code of the project

---
+++ Spring-project-2/src/main/java/com/example/Spring/project2/entities/Subject.java
@@ -0,0 +1,32 @@
+package com.example.Spring.project2.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @Column(unique = true)
+    private String name;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "enrolSubjectsToStudents", cascade = CascadeType.ALL)
+    private Set<Student> students;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "enrolSubjectsToTeachers", cascade = CascadeType.ALL)
+    private Set<Teacher> teachers;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private School enrolSubjectToSchool;
+}
---
+++ Spring-project-2/src/main/java/com/example/Spring/project2/entities/Teacher.java
@@ -0,0 +1,58 @@
+package com.example.Spring.project2.entities;
+
+import com.example.Spring.project2.enums.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String name;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    private LocalDate dateOfBirth;
+    @Transient
+    private int age;
+
+    public int getAge() {
+        return Period.between(dateOfBirth, LocalDate.now()).getYears();
+    }
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "system_id")
+    private User user;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private School enrolTeachersToSchool;
+
+    @OneToMany(mappedBy = "teacher", cascade = CascadeType.ALL)
+//    @JoinColumn(name = "students", referencedColumnName = "name")
+    private List<Student> students;
+
+    @JsonIgnore
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "subject_enrolled",
+            joinColumns = @JoinColumn(name = "teacher"),
+            inverseJoinColumns = @JoinColumn(name = "subject")
+    )
+    private Set<Subject> enrolSubjectsToTeachers = new HashSet<>();
+}
